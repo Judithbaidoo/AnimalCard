45636709e9bc1abd38f1dd4d78d6173aafc40dfa
Judith Baidoo <judithbaidoo@Judiths-MacBook-Pro-2.local>
2023-01-28 23:19:55 +0000

    changing styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="border1" >
+		        
+			<h2>Cat</h2>
+			<ul>
+				<li><span>Scientific name</span>:Felis catus</li>
+				<li><span>Longest cat recorded</span>:48.5 inches</li>
+				<li><span>Average Lifespan</span>: 2-16 years</li>
+				<li><span>Habitat</span>: warm,dry areas/unknown</li>
+			  </ul>
+
+		    
+		
+		    <img className="image" src = "https://images.unsplash.com/photo-1593483316242-efb5420596ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" alt="cat"/>
+
+		
+		
+
+		
+		
+			
+			<div className="border2">
+				<p >
+				<em>Originally it was thought that Egyptians domesticated the cat but in 2004,
+				French archaeologists discovered a 9,500 year old cat grave in Cyprus.</em></p>
+			
+		
+
+		       
+		
+				
+		
+			
+			  
+			
+			  <p>The cat (Felis catus) is a domestic species of small carnivorous mammal.
+			   It is the only domesticated species in the family Felidae and is often referred to as 
+				the domestic cat to distinguish it from the wild members of the family.</p>
+			
+			
+			  </div>
+	</div>
   );
 }
 
